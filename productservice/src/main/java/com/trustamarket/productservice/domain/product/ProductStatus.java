9a0e2f3d82370e4f9e1705983baa78cd22d93441
kwanhyoung <kimgh961021@gmail.com>
2026-04-23 15:28:43 +0900

    feat: 상품 등급 및 상태 enum 추가

---
+++ productservice/src/main/java/com/trustamarket/productservice/domain/product/ProductStatus.java
@@ -0,0 +1,20 @@
+package com.trustamarket.productservice.domain.product;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ProductStatus {
+
+    ON_SALE("판매중"),
+    RESERVED("예약중"),
+    SOLD_OUT("판매완료");
+
+    private final String description;
+
+    // 상품구매가능 확인
+    public boolean isAvailableForOrder() {
+        return this == ON_SALE;
+    }
+}
