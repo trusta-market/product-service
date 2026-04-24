9fb4949c5a22487b370c0fa68bfb7e3f06e8431f
kwanhyoung <kimgh961021@gmail.com>
2026-04-24 12:01:45 +0900

    feat: ProductRepository 구현 - 판매자/카테고리별 상품 조회 시 Pageable 기반 페이징 적용 - 최근에 등록된 상품 조회를 위한 쿼리 메서드 추가

---
+++ productservice/src/main/java/com/trustamarket/productservice/domain/product/ProductRepository.java
@@ -0,0 +1,29 @@
+package com.trustamarket.productservice.domain.product;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+    @EntityGraph(attributePaths = {"images"})
+    @Query("select p from Product p where p.id = :id")
+    Optional<Product> findByIdWithImages(@Param("id") Long id);
+
+    // 특정 판매자의 모든 판매 목록
+    Page<Product> findBySellerId(Long sellerId, Pageable pageable);
+
+    //특정 카테고리 판매 목록
+    Page<Product> findByCategoryId(Long categoryId, Pageable pageable);
+
+    // 최근 등록일 기준 조회
+    @EntityGraph(attributePaths = {"images"})
+    List<Product> findTop10ByOrderByCreatedAtDesc();
+}
