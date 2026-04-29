3bb3c87837fc7d0a7dec564121aaa2fbc212f11d
kwanhyoung <kimgh961021@gmail.com>
2026-04-29 14:11:17 +0900

    fix: id타입 변경 - UUID로 변경

--- productservice/src/main/java/com/trustamarket/productservice/application/exception/CategoryNotFoundException.java
+++ productservice/src/main/java/com/trustamarket/productservice/application/exception/CategoryNotFoundException.java
@@ -4,8 +4,10 @@ package com.trustamarket.productservice.application.exception;
 import com.trustamarket.common.exception.NotFoundException;
 import com.trustamarket.productservice.application.exception.errorcode.ProductErrorCode;
 
+import java.util.UUID;
+
 public class CategoryNotFoundException extends NotFoundException {
-    public CategoryNotFoundException(Long id) {
+    public CategoryNotFoundException(UUID id) {
         super(ProductErrorCode.CATEGORY_NOT_FOUND.getMessage() + " id: " + id);
     }
 }
--- productservice/src/main/java/com/trustamarket/productservice/application/exception/ProductNotFoundException.java
+++ productservice/src/main/java/com/trustamarket/productservice/application/exception/ProductNotFoundException.java
@@ -3,8 +3,10 @@ package com.trustamarket.productservice.application.exception;
 import com.trustamarket.common.exception.NotFoundException;
 import com.trustamarket.productservice.application.exception.errorcode.ProductErrorCode;
 
+import java.util.UUID;
+
 public class ProductNotFoundException extends NotFoundException {
-    public ProductNotFoundException(Long id) {
+    public ProductNotFoundException(UUID id) {
         super(ProductErrorCode.PRODUCT_NOT_FOUND.getMessage() + " id: " + id);
     }
 }
