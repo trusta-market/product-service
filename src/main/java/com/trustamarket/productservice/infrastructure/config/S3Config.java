e63e710663f9b5121575bd348463b3e61ffc443c
kwanhyoung <kimgh961021@gmail.com>
2026-05-14 18:07:46 +0900

    refactor: S3 리전 하드코딩 제거 - 환경별 배포 유연성을 위해 "ap-southeast-2" 하드코딩 상수 제거

--- src/main/java/com/trustamarket/productservice/infrastructure/config/S3Config.java
+++ src/main/java/com/trustamarket/productservice/infrastructure/config/S3Config.java
@@ -1,16 +1,15 @@
 package com.trustamarket.productservice.infrastructure.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-import org.springframework.beans.factory.annotation.Value;
-import software.amazon.awssdk.services.s3.S3Configuration;
 
-import java.net.URI;
 
 @Configuration
 public class S3Config {
@@ -24,12 +23,14 @@ public class S3Config {
     @Value("${cloud.aws.region.static}")
     private String region;
 
-    private static final String REGION = "ap-southeast-2";
-
     @Bean
     public S3Client s3Client() {
+        if (!StringUtils.hasText(region)) {
+            throw new IllegalStateException(
+                    "cloud.aws.region.static 설정이 누락되었습니다. S3 리전을 config에 명시해주세요.");
+        }
         return S3Client.builder()
-                .region(Region.of(REGION))
+                .region(Region.of(region))
                 .credentialsProvider(awsCredentialsProvider())
                 .build();
     }
