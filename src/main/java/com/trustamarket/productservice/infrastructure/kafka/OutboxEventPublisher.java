84715fb814ba3ae44e05708b0ef5ad4f40307170
kwanhyoung <kimgh961021@gmail.com>
2026-06-11 15:29:43 +0900

    fix:  OutboxEventPublisher에서 ShedLock 관련 코드 삭제

--- src/main/java/com/trustamarket/productservice/infrastructure/kafka/OutboxEventPublisher.java
+++ src/main/java/com/trustamarket/productservice/infrastructure/kafka/OutboxEventPublisher.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+
 import java.util.List;
 
 @Slf4j
